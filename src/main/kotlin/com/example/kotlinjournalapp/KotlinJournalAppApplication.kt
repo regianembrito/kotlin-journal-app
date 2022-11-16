7418da18df542e1047f44a719f1e210b560e6261
Regiane Martins de Brito <martins.regiane.silva@gmail.com>
2022-11-16 20:44:32 -0300

    First big commit

---
+++ src/main/kotlin/com/example/kotlinjournalapp/KotlinJournalAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinjournalapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJournalAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinJournalAppApplication>(*args)
+}
