8fd46ba667bc5cb5600711784c236172a3ade9ff
Regiane Martins de Brito <martins.regiane.silva@gmail.com>
2022-11-16 22:21:31 -0300

    Journal JPA class

---
+++ src/main/kotlin/com/example/kotlinjournalapp/models/Journal.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinjournalapp.models
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@Entity(name = "journals")
+data class Journal(
+    @Id @GeneratedValue(
+        strategy = GenerationType.IDENTITY) val id : Long = 0,
+    @get: NotBlank val title : String = "",
+    @get: NotBlank val content : String = ""){
+}
